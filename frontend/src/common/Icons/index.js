94c39c801ac069ed590b92c5faf122ec6a447eeb
Roman Franko <roma.franko99@gmail.com>
2022-04-25 02:09:25 +0300

    Basic project part

--- frontend/src/common/Icons/index.js
+++ frontend/src/common/Icons/index.js
@@ -1,5 +1,6 @@
 import Mail from "./Mail";
 import LinkedIn from "./LinkedIn";
 import Facebook from "./Facebook";
+import Github from "./Github";
 
-export { Mail, LinkedIn, Facebook };
+export { Mail, LinkedIn, Facebook, Github };
