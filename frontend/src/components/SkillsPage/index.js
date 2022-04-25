801d88ba8841f2d5ade97703ec88256ae75882ad
Roman Franko <roma.franko99@gmail.com>
2022-04-25 20:51:00 +0300

    skill page | listing skills

--- frontend/src/components/SkillsPage/index.js
+++ frontend/src/components/SkillsPage/index.js
@@ -3,41 +3,15 @@ import { AppContext } from "../AppContext";
 import SkillElement from "./SkillElement";
 import styles from "./SkillsPage.module.css";
 
-const content = [
-  {
-    skillLabel: "JavaScript",
-    level: 90,
-    description:
-      " Lorem ipsum dolor sit amet consectetur adipisicing lit. Ducimus nihil possimus sed eum eaque debitis, cupiditate quos maiores molestias suscipit recusandae reprehenderit mollitia sint quo asperiores saepe! Ipsum, optio illum.",
-    knowledgeList: ["Webpack", "React", "Vue", "Storybook"],
-  },
-  {
-    skillLabel: "React",
-    level: 90,
-  },
-];
-
-const skillList = [
-  {
-    label: "Programing",
-    childSkills: content,
-  },
-  {
-    label: "Languages",
-    childSkills: content,
-  },
-];
-
 const SkillsPage = () => {
   const { skills } = useContext(AppContext);
-  console.log(skills);
   return (
     <div>
       <h1>SkillsPage</h1>
       <div className={styles.contentWrapper}>
-        {skillList.map(({ label, childSkills }) => {
+        {skills.map(({ label, content }) => {
           return (
-            <SkillElement key={label} label={label} childSkills={childSkills} />
+            <SkillElement key={label} label={label} childSkills={content} />
           );
         })}
       </div>
