9edd15cf8d8d885135a9f5e1bb823c87c00c509a
Roman Franko <roma.franko99@gmail.com>
2022-04-25 16:04:26 +0300

    backend | refactor main root

---
+++ backend/common/googleSheetApi.js
@@ -0,0 +1,14 @@
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const token = require("../token.json");
+
+async function getSheet(sheetName) {
+  const doc = new GoogleSpreadsheet(token.sheet_id);
+  await doc.useServiceAccountAuth({
+    client_email: token.client_email,
+    private_key: token.private_key,
+  });
+  await doc.loadInfo();
+  return (sheet = doc.sheetsByTitle[sheetName]);
+}
+
+module.exports = getSheet;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,21 +1,9 @@
-const router = require('express').Router();
-const { GoogleSpreadsheet } = require('google-spreadsheet');
-const token = require('../token.json')
-const transformData = require('../helpers/transformRows')
+const router = require("express").Router();
+const getSheet = require("../common/googleSheetApi");
+const transformData = require("../helpers/transformRows");
 
-async function getSheet(){
-  const doc = new GoogleSpreadsheet(token.sheet_id);
-  await doc.useServiceAccountAuth({
-    client_email: token.client_email,
-    private_key: token.private_key,
-  });
-  await doc.loadInfo(); 
-  const sheet = doc.sheetsByTitle["projects"]; 
-  return transformData(sheet)
-}
-
-router.get('/', async function(req, res, next) {
-  res.json(await getSheet());
+router.get("/projects", async function (req, res, next) {
+  res.json(await transformData(await getSheet("projects")));
 });
 
 module.exports = router;
