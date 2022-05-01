672d8ccf91f7b6d426e4d8028b98b87b2c114108
Roman Franko <roma.franko99@gmail.com>
2022-05-01 14:45:04 +0300

    Frontend | svg + animation

--- frontend/src/components/Root/index.js
+++ frontend/src/components/Root/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import ProfilePage from "../ProfilePage";
 import AppLayout from "../AppLayout";
 import ProjectsPage from "../ProjectsPage";
