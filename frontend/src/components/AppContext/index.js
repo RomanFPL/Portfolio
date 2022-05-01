1f23797c719e25391b497887523a122c90eab0f6
Roman Franko <roma.franko99@gmail.com>
2022-05-01 15:10:49 +0300

    frontend | add process env

--- frontend/src/components/AppContext/index.js
+++ frontend/src/components/AppContext/index.js
@@ -5,21 +5,17 @@ import Spinner from "../Spinner";
 export const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
+  const domain = process.env.REACT_APP_API_SERVICE;
   const [menuOpened, setMenuOpened] = useState(true);
   const [projectData, setProjectData] = useState(undefined);
   const [skillData, setSkillData] = useState(undefined);
   const [menuData, setMenuData] = useState(undefined);
+
   useEffect(() => {
-    getApiData("http://localhost:3001/skills").then((data) =>
-      setSkillData(data)
-    );
-    getApiData("http://localhost:3001/projects").then((data) =>
-      setProjectData(data)
-    );
-    getApiData("http://localhost:3001/menu").then((data) =>
-      setMenuData(data)
-    );
-  }, []);
+    getApiData(domain + "skills").then((data) => setSkillData(data));
+    getApiData(domain + "projects").then((data) => setProjectData(data));
+    getApiData(domain + "menu").then((data) => setMenuData(data));
+  }, [domain]);
 
   if (!projectData || !skillData || !menuData) {
     return <Spinner />;
