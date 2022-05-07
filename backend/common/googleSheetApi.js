159f903e46a045dd9a387f23b090fe7702dca963
Roman Franko <roma.franko99@gmail.com>
2022-05-07 23:28:30 +0300

    backend | error handling

--- backend/common/googleSheetApi.js
+++ backend/common/googleSheetApi.js
@@ -2,13 +2,17 @@ const { GoogleSpreadsheet } = require("google-spreadsheet");
 const token = require("../token.json")
 
 async function getSheet(sheetName) {
-  const doc = new GoogleSpreadsheet(token.sheet_id);
-  await doc.useServiceAccountAuth({
-    client_email: token.client_email,
-    private_key: token.private_key,
-  });
-  await doc.loadInfo();
-  return (sheet = doc.sheetsByTitle[sheetName]);
+  try{
+    const doc = new GoogleSpreadsheet(token.sheet_id);
+    await doc.useServiceAccountAuth({
+      client_email: token.client_email,
+      private_key: token.private_key,
+    });
+    await doc.loadInfo();
+    return (sheet = doc.sheetsByTitle[sheetName]);
+  } catch(e) {
+    throw new Error('There no tokens in your app' + e)
+  }
 }
 
 module.exports = getSheet;
