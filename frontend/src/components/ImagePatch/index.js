2d6c844804fdf5ed0b7db31a3e19aa9f87028abe
Roman Franko <roma.franko99@gmail.com>
2022-08-01 00:35:51 +0300

    frontend: add image preloder

---
+++ frontend/src/components/ImagePatch/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import defaultProject from "../../common/noImg.svg";
+
+const ImagePatch = () => {
+  return <img width="100px" src={defaultProject} alt="No project" />;
+};
+
+export default ImagePatch;
--- frontend/src/components/ProjectsPage/ProjectItem.js
+++ frontend/src/components/ProjectsPage/ProjectItem.js
@@ -1,11 +1,27 @@
 import styles from "./ProjectsPage.module.css";
-import defaultProject from "../../common/coming.jpg";
 import { Github } from "../../common/Icons";
 import ProgressBar from "../ProgressBar";
+import { useState } from "react";
+import classNames from "classnames";
+import ImagePatch from "../ImagePatch";
 
-const ProjectItem = ({
-  project: { label, description, githubLink, projectPreviewLink, progress, imgLink },
-}) => {
+const ProjectItem = ({ project }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const onLoad = () => {
+    setIsLoaded(true);
+  };
+
+  if (!project) return null;
+
+  const {
+    label,
+    description,
+    githubLink,
+    projectPreviewLink,
+    progress,
+    imgLink,
+  } = project;
+
   const githubPreview = githubLink && (
     <a
       className={styles.githubLink}
@@ -16,8 +32,6 @@ const ProjectItem = ({
       <Github />
     </a>
   );
-
-  console.log(imgLink)
 
   return (
     <div className={styles.projectWrapper}>
@@ -33,7 +47,15 @@ const ProjectItem = ({
       <div className={styles.visualPart}>
         <div className={styles.previewScreen}>
           <a href={projectPreviewLink}>
-            <img src={imgLink || defaultProject} alt={label} />
+            <img
+              className={classNames(styles.previewScreenImg, {
+                [styles.hideImg]: !isLoaded,
+              })}
+              onLoad={onLoad}
+              src={imgLink}
+              alt={label}
+            />
+            {!isLoaded && <ImagePatch />}
           </a>
         </div>
       </div>
