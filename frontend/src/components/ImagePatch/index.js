ed3db879bbbb3346de9f96c1714033e706fdb63f
Roman Franko <roma.franko99@gmail.com>
2022-08-02 20:01:32 +0300

    frontend: Image as component

--- frontend/src/components/ImagePatch/index.js
+++ frontend/src/components/ImagePatch/index.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import defaultProject from "../../common/noImg.svg";
+import classNames from "classnames";
 
-const ImagePatch = () => {
+import styles from "./ImagePatch.module.css";
+
+const ImagePatch = ({imgLink, label}) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const onLoad = () => {
+    setIsLoaded(true);
+  };
+  
+  if(imgLink){
+    return (
+      <img
+      className={classNames(styles.previewScreenImg, {
+        [styles.hideImg]: !isLoaded,
+      })}
+      onLoad={onLoad}
+      src={imgLink}
+      alt={label}
+    />
+    )
+  }
+  
   return <img width="100px" src={defaultProject} alt="No project" />;
 };
 
--- frontend/src/components/ProjectsPage/ProjectItem.js
+++ frontend/src/components/ProjectsPage/ProjectItem.js
@@ -1,16 +1,9 @@
 import styles from "./ProjectsPage.module.css";
 import { Github } from "../../common/Icons";
 import ProgressBar from "../ProgressBar";
-import { useState } from "react";
-import classNames from "classnames";
 import ImagePatch from "../ImagePatch";
 
 const ProjectItem = ({ project }) => {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const onLoad = () => {
-    setIsLoaded(true);
-  };
-
   if (!project) return null;
 
   const {
@@ -47,15 +40,7 @@ const ProjectItem = ({ project }) => {
       <div className={styles.visualPart}>
         <div className={styles.previewScreen}>
           <a href={projectPreviewLink}>
-            <img
-              className={classNames(styles.previewScreenImg, {
-                [styles.hideImg]: !isLoaded,
-              })}
-              onLoad={onLoad}
-              src={imgLink}
-              alt={label}
-            />
-            {!isLoaded && <ImagePatch />}
+            <ImagePatch imgLink={imgLink} label={label}/>
           </a>
         </div>
       </div>
