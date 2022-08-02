02819ffdb7ffebf1ce02bac4762544c211a247c4
Roman Franko <roma.franko99@gmail.com>
2022-08-02 20:11:36 +0300

    frontend: Spinner separated

--- frontend/src/components/Spinner/index.js
+++ frontend/src/components/Spinner/index.js
@@ -2,11 +2,9 @@ import styles from "./Spinner.module.css";
 
 const Spinner = () => {
   return (
-    <div className={styles.centerBody}>
-      <div className={styles.loaderCircle}>
-        Loading
-        <span></span>
-      </div>
+    <div className={styles.loaderCircle}>
+      Loading
+      <span></span>
     </div>
   );
 };
---
+++ frontend/src/components/SpinnerPage/index.js
@@ -0,0 +1,12 @@
+import Spinner from "../Spinner";
+import styles from "./Spinner.module.css";
+
+const SpinnerPage = () => {
+  return (
+    <div className={styles.centerBody}>
+      <Spinner />
+    </div>
+  );
+};
+
+export default SpinnerPage;
